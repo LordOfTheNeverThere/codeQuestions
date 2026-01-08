2278f438fa153bdda808688e266694b4ef5de529
miguel <1241690@isep.ipp.pt>
2026-01-08 19:59:54 +0000

    exercise 169

--- c++/leetcode/169/main.cpp
+++ c++/leetcode/169/main.cpp
@@ -1,23 +1,70 @@
-#include <array>
 #include <iostream>
 #include <vector>
 
 
+using Int = int32_t;
+
+struct UnaryCounter {
+    Int number;
+    Int freq;
+};
+
+class NumberFreqCounter {
+private:
+    std::vector<UnaryCounter> mCounter{UnaryCounter {0,0}};
+public:
+    Int findNumber(Int query) const {
+        for (int i = 0; i < mCounter.size(); ++i) {
+            if (mCounter[i].number == query) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    void incrementUnaryCounter(Int index, Int number) {
+        if (index == -1) {
+            mCounter.push_back(UnaryCounter {number , 1});
+        } else {
+            mCounter[index].freq++;
+        }
+
+    }
+
+    Int& getNumberFreq(Int index) {
+        if (index==-1) {
+            return mCounter.back().freq;
+        }
+
+        return mCounter[index].freq;
+    }
+
+    Int& getNumber(Int index) {
+        if (index==-1) {
+            return mCounter.back().number;
+        }
+        return mCounter[index].number;
+    }
+};
 
 
 class Solution {
 public:
-    int majorityElement(std::vector<int>& nums) {
+    Int majorityElement(std::vector<Int>& nums) {
         auto size {nums.size()};
-        int moreThanHalf { static_cast<int>((size +1)/ 2) };
+        Int moreThanHalf { static_cast<Int>((size +1)/ 2) };
+        NumberFreqCounter resCounter {};
 
-        std::array<int,10> counter {0,0,0,0,0,0,0,0,0,0};
+        for (Int num: nums) {
+            Int counterIndex {resCounter.findNumber(num)};
+            resCounter.incrementUnaryCounter(counterIndex, num);
 
-        for (int num: nums) {
-            if (moreThanHalf==++counter[num])
-                return num;
+            if (resCounter.getNumberFreq(counterIndex) == moreThanHalf) {
+                return resCounter.getNumber(counterIndex);
+            }
         }
-        return -1;
+
+        return -1; //No majority Element found (should not happen)
     }
 };
 
