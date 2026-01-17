cee9c341a9e3994b705d8dd41a906139c7f401aa
miguel <1241690@isep.ipp.pt>
2026-01-17 19:37:55 +0000

    Add Solution to problem 189

--- c++/leetcode/189/main.cpp
+++ c++/leetcode/189/main.cpp
@@ -7,22 +7,72 @@ class Solution {
 public:
     static void rotate(std::vector<Int>& nums, Int k) {
 
+
+
         Int const size = static_cast<Int>(nums.size());
-        
+        std::vector<Int> tempVector {};
 
-        for (Int i = 0; i < size - k - 1; ++i) {
+        while (k >= size) {
+            k = k - size;
+        }
+
+        if (k > size/2) {
+            for (int i = 0; i < k; ++i) {
+                tempVector.push_back(nums[i]);
+            }
+
+            for (int i = k; i < size; ++i) {
+                if ((i + k) >= size) {
+                    nums[i + k - size] = nums[i];
+                } else {
+                    nums[i + k] = nums[i];
+                }
+            }
+
+            for (int i = 0; i < k; ++i) {
+
+                if ((i + k) >= size) {
+                    nums[i + k - size] = tempVector[i];
+                }
+                else {
+                    nums[i + k] = tempVector[i];
+                }
+            }
+
+        } else {
+
+
+            for (int i = k; i < size; ++i) {
+                tempVector.push_back(nums[i]);
+            }
+
+            for (int i = 0; i < k; ++i) {
+                if ((i + k) >= size) {
+                    nums[i + k - size] = nums[i];
+                } else {
+                    nums[i + k] = nums[i];
+                }
+            }
+
+            for (int i = k; i < size; ++i) {
+                if (i + k >= size) {
+                    nums[i + k - size] = tempVector[i - k];
+                } else {
+                    nums[i + k] = tempVector[i - k];
+                }
+
+            }
 
         }
 
-
-        for (Int i = size - k - 1; i < size; ++i) {
-
-        }
 
     }
 };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<Int> vector {1,2,3};
+    Solution::rotate(vector, 2);
+
+    std::cout << vector[0] << std::endl;
     return 0;
 }
