e573e1a6d0b445fb35edfaa93fd8c6e8c504f128
miguel <1241690@isep.ipp.pt>
2026-01-17 22:41:51 +0000

    Start 238

--- c++/leetcode/238/main.cpp
+++ c++/leetcode/238/main.cpp
@@ -1,4 +1,24 @@
 #include <iostream>
+#include <vector>
+
+using Int = int32_t;
+
+class Solution {
+public:
+    std::vector<Int> productExceptSelf(std::vector<Int>& nums) {
+        std::vector<Int> result (nums.size());
+        Int product {1};
+        for (int i = 0; i < nums.size(); ++i) {
+            product = product * nums[i];
+        }
+        for (Int num: nums) {
+            result.push_back(product - num);
+        }
+
+        return result;
+    }
+};
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
