56e9797f8587b1e3f174291672fe9d3ee038ce8a
miguel <1241690@isep.ipp.pt>
2026-01-18 22:10:56 +0000

    Start 122

---
+++ c++/leetcode/122/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using Int = int32_t;
+
+class Solution {
+public:
+    int maxProfit(std::vector<Int>& prices) {
+
+
+
+    }
+};
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
