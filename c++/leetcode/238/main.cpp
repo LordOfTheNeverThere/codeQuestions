2b299ffa26c4fab0b1b311f04be01d3c91f64017
LordOfTheNeverThere <miguel.dinis.s@tecnico.ulisboa.pt>
2026-01-18 07:34:50 +0000

    Finished 238

--- c++/leetcode/238/main.cpp
+++ c++/leetcode/238/main.cpp
@@ -1,4 +1,6 @@
+#include <cstdint>
 #include <iostream>
+#include <unordered_set>
 #include <vector>
 
 using Int = int32_t;
@@ -6,19 +8,39 @@ using Int = int32_t;
 class Solution {
 public:
     std::vector<Int> productExceptSelf(std::vector<Int>& nums) {
-        std::vector<Int> result (nums.size());
+        std::vector<Int> result {};
+        std::pmr::unordered_set<Int> zeroPositions {};
         Int product {1};
         for (int i = 0; i < nums.size(); ++i) {
-            product = product * nums[i];
+
+            if (nums[i] == 0) {
+                zeroPositions.insert(i);
+            } else {
+                product = product * nums[i];
+            }
+
+            if (zeroPositions.size() >= 2) {
+                product = 0;
+                break;
+            }
         }
-        for (Int num: nums) {
-            result.push_back(product - num);
+        for (Int i=0; i < nums.size(); ++i) {
+            if (zeroPositions.size() >= 2) {
+                result.push_back(0);
+
+            } else if (zeroPositions.find(i) != zeroPositions.end()) {
+                result.push_back(product / 1);
+            } else if (zeroPositions.size() == 1) {
+                result.push_back(0);
+            } else {
+                result.push_back(product / nums[i]);
+            }
+
         }
 
         return result;
     }
 };
-
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
