26de0c22aa259f2aa8858b934ee02f5af6893892
miguel <1241690@isep.ipp.pt>
2026-01-19 23:23:18 +0000

    Start 2348

---
+++ c++/leetcode/2348/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using Int = int32_t;
+
+class Solution {
+public:
+    long long zeroFilledSubarray(std::vector<Int>& nums) {
+        Int currentSubArraySize {0};
+        long long numOfSubArrays {0};
+
+        for (Int num: nums) {
+            if (num == 0) {
+                if (currentSubArraySize != 0) {
+                    numOfSubArrays++;
+                } 
+                numOfSubArrays++;
+                currentSubArraySize++;
+            }
+            else {
+                currentSubArraySize = 0;
+            }
+        }
+        return numOfSubArrays;
+    }
+};
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
