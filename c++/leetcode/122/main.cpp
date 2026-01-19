2d942cb23937399e7d63f436a0d885aafeabd085
miguel <1241690@isep.ipp.pt>
2026-01-19 23:04:36 +0000

    Finish question 122

--- c++/leetcode/122/main.cpp
+++ c++/leetcode/122/main.cpp
@@ -5,15 +5,37 @@ using Int = int32_t;
 
 class Solution {
 public:
-    int maxProfit(std::vector<Int>& prices) {
+    Int static maxProfit(std::vector<Int>& prices) {
 
+        Int currStockIndex {-1};
+        Int profit {0};
 
+        for (Int i = 0; i < prices.size(); ++i) {
+
+            if (i == prices.size() - 1 && (currStockIndex != -1)) {
+                profit = profit + (prices[i] - prices[currStockIndex]);//Sell
+                currStockIndex = -1;
+
+            } else if (i == prices.size() - 1 && (currStockIndex == -1)) {
+                break; // Should not buy on the last day
+
+            } else if (currStockIndex == -1 && (prices[i] < prices[i+1])) {
+                currStockIndex = i; //Buy
+
+            } else if (currStockIndex != -1 && (prices[i] > prices[i+1])) {
+                profit = profit + (prices[i] - prices[currStockIndex]);//Sell
+                currStockIndex = -1;
+            }
+        }
+        return profit;
 
     }
 };
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    std::vector<Int> prices {2,1,2,0,1};
+    std::cout << Solution::maxProfit(prices) << std::endl;
     return 0;
 }
