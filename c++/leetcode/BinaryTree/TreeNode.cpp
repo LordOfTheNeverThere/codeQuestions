789f37a623cf0a0896ea4d28b36db1714bdb7b6b
LordOfTheNeverThere <miguel.dinis.s@tecnico.ulisboa.pt>
2026-01-25 08:07:49 +0000

    Changes on the BST delete method

--- c++/leetcode/BinaryTree/TreeNode.cpp
+++ c++/leetcode/BinaryTree/TreeNode.cpp
@@ -24,18 +24,17 @@ public:
     }
 
     void insert(Int value) {
-        auto newNode {std::make_unique<TreeNode>(value)};
 
         if (value < m_value) {
             if (!m_left) {
-                m_left = std::move(newNode);
+                m_left = std::move(std::make_unique<TreeNode>(value));
             } else {
                 m_left->insert(value);
             }
 
         } else if (value > m_value) {
             if (!m_right) {
-                m_right = std::move(newNode);
+                m_right = std::move(std::make_unique<TreeNode>(value));
             } else {
                 m_right->insert(value);
             }
@@ -45,9 +44,9 @@ public:
     TreeNode* find(Int value) {
         if (value == m_value) {
             return this;
-        } else if (!m_left && value < m_value) {
+        } else if (m_left && value < m_value) {
             return m_left->find(value);
-        } else if (!m_right && value > m_value) {
+        } else if (m_right && value > m_value) {
             return m_right->find(value);
         } else {
             //this is a leaf, the end ...
@@ -55,18 +54,37 @@ public:
         }
     }
 
-    void remove(Int value) {
+    TreeNode* remove(Int value) {
 
-        if (!m_left && m_left.get()->getValue() == value) {
-            
+        if (value == m_value) {
+            if (m_left && m_right) { // Has both children
+                // in this case in order to not break the BST condition we must substitue this node with
+                // the one with the lesser value of the m_right subtree
+                m_value = m_right->findAndRemoveBSTMin()->getValue();
 
-        } else if (!m_right && m_right.get()->getValue() == value) {
+            } else if (m_left) { // Has Leftmost children
+                return m_left.get();
+            }
+            else if (m_right) { // Has Rightmost children
+                return m_right.get();
+            }
+            else { // Has no children = Leaf node
+                return nullptr;
+            }
+        } else if (m_left && value < m_value) {
+            m_left->remove(value);
+        } else if (m_right && value > m_value) {
+            m_right->remove(value);
+        }
+    }
 
-        } else if (!m_left && m_value > value ) {
-            m_left.get()->remove(value);
-
-        } else if (!m_right && m_value < value ) {
-            m_right.get()->remove(value);
+    TreeNode* findAndRemoveBSTMin() {
+        if (!(m_left->getLeft())) {
+            return m_left->findAndRemoveBSTMin();
+        } else {
+            TreeNode* min = m_left.get();
+            m_left.reset(m_left->getRight());
+            return min;
         }
     }
 };
