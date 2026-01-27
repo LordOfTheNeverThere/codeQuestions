4f77d98ba9d76ad076e5e93c6d2f41d142ea160f
LordOfTheNeverThere <miguel.dinis.s@tecnico.ulisboa.pt>
2026-01-27 23:36:41 +0000

    Correct remove

--- c++/leetcode/BinaryTree/TreeNode.cpp
+++ c++/leetcode/BinaryTree/TreeNode.cpp
@@ -34,7 +34,7 @@ public:
         }
     }
 
-    TreeNode* find(Int value) {
+    TreeNode* find(const Int value) {
         if (value == m_value) {
             return this;
         } else if (m_left && value < m_value) {
@@ -59,41 +59,32 @@ public:
         }
     }
 
-    void lumberjack(TreeNode* nodeToCut, Int value) {
+    void lumberjack(std::unique_ptr<TreeNode>& nodeToCut, const Int value) {
         if (nodeToCut->getValue() == value) {
             if ((nodeToCut->getLeft()) && (nodeToCut->getRight())) { // Has both children
                 // in this case in order to not break the BST condition we must substitue this node with
                 // the one with the lesser value of the m_right subtree
                 nodeToCut->m_value = nodeToCut->getRight()->findAndRemoveBSTMin();
             } else if (nodeToCut->getLeft()) { // Has Leftmost children
-                nodeToCut = nodeToCut->getLeft();
+                nodeToCut = std::move(nodeToCut->m_left);
             }
             else if (nodeToCut->getRight()) { // Has Rightmost children
-                nodeToCut = nodeToCut->getRight();
+                nodeToCut = std::move(nodeToCut->m_right);
             }
             else { // Has no children = Leaf node
-                nodeToCut = nullptr;
+                nodeToCut.reset(nullptr);
             }
         } else {
             nodeToCut->remove(value);
         }
     }
 
-    TreeNode* remove(Int value) {
-
-        TreeNode* nodeToCut {nullptr};
-
-        if (value < m_value) {
-            nodeToCut = m_left.get();
-
-        } else if (value > m_value) {
-            nodeToCut = m_right.get();
+    void remove(const Int value) {
+        if (value < m_value && m_left) {
+            lumberjack(m_left, value);
+        } else if (value > m_value && m_right) {
+            lumberjack(m_right, value);
         }
-
-        if (nodeToCut) {
-            lumberjack(nodeToCut, value);
-        }
-
     }
 };
 
